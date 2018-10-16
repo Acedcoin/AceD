032d833279306fa7eee743846e0f8d38a5c9d643
helios <helios@polispay.org>
2018-10-16 11:53:39 -0500

    Testing update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -162,7 +162,7 @@ public:
             consensus.nPoSDiffAdjustRange = 1;
 
             // POS hard fork date
-            consensus.nLastPoWBlock = 203248;
+            consensus.nLastPoWBlock = 203260;
 
             consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
             consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
