6339a1646698deafd711fa6c94fd09d9936ac383
helios <helios@polispay.org>
2018-10-17 10:34:06 -0500

    Moved block to 203920

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -160,7 +160,7 @@ public:
             consensus.nPoSDiffAdjustRange = 1;
 
             // POS hard fork date
-            consensus.nLastPoWBlock = 203900;
+            consensus.nLastPoWBlock = 203920;
 
             consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
             consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
