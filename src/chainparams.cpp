fe092dbc4285dedcc41f0c8d4ff33dca43a7068d
Cronos <cronos@polispay.org>
2018-10-17 16:32:43 -0500

    Bump

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -160,7 +160,7 @@ public:
             consensus.nPoSDiffAdjustRange = 5;
 
             // POS hard fork date
-            consensus.nLastPoWBlock = 204065;
+            consensus.nLastPoWBlock = 204075;
 
             consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
             consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
