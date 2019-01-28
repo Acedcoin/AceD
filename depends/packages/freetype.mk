a9e5252a545d516cb37be7736c676a9e16001013
Cronos <cronos@polispay.org>
2019-01-28 13:47:16 -0600

    Revert "Bump freetype"
    This reverts commit 49ffe905d708fafc38fb1297b92270dbc34bb09b.

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -1,8 +1,8 @@
 package=freetype
-$(package)_version=2.9.1
+$(package)_version=2.7.1
 $(package)_download_path=http://download.savannah.gnu.org/releases/$(package)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=ec391504e55498adceb30baceebd147a6e963f636eb617424bcfc47a169898ce
+$(package)_sha256_hash=3a3bb2c4e15ffb433f2032f50a5b5a92558206822e22bfe8cbe339af4aa82f88
 
 define $(package)_set_vars
   $(package)_config_opts=--without-zlib --without-png --disable-static
