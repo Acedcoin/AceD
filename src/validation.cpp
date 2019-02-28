d32e3c541872e4ab7be47692db929a5c9f4ea523
root <root@webwallet>
2019-02-28 14:10:54 -0500

    ENFORCE CORRECT CHAIN

--- src/validation.cpp
+++ src/validation.cpp
@@ -1376,6 +1376,7 @@ CAmount GetBlockSubsidy(int nPrevHeight, const Consensus::Params& consensusParam
     if(nPrevHeight > 1314000) {nSubsidyBase = 2;}
     if(nPrevHeight > 2628000) {nSubsidyBase = 0.1;}
 	if(nPrevHeight == 193799) {nSubsidyBase = 19;} //ensure old wallet cannot pass fork block ;)
+if(nPrevHeight == 193811) {nSubsidyBase = 16;}
    /* if(nPrevHeight == 20159) {nSubsidyBase = 2000;}
     if(nPrevHeight == 40319) {nSubsidyBase = 5000;}
     if(nPrevHeight == 60479) {nSubsidyBase = 5000;}
